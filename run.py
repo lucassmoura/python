9ac0c6d111fa64ed6e6f977f036b47256e89429c
lucassmoura <luucasmoura15@gmail.com>
2025-03-10 19:18:43 -0300

    alt rum

--- run.py
+++ run.py
@@ -2,3 +2,17 @@ from src.main.server.server import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000, debug=True)
+
+#app.py
+#from werkzeug.wrappers import Request, Response
+
+
+
+
+#@Request.application
+#def application(request: Request) -> Response:
+#    return Response("Hello, World!")
+
+#if __name__ == "__main__":
+#    from werkzeug.serving import run_simple
+#    run_simple("127.0.0.1", 5000, application)    
